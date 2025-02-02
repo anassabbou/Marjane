76de83753766721dd9a30c692b08a0a61ac708af
anass abbou <anass.abbou1@usms.ac.ma>
2025-02-02 19:12:46 +0100

    Add Entities and Configure Database

---
+++ src/main/java/com/abbou/marjane/MarjaneApplication.java
@@ -0,0 +1,13 @@
+package com.abbou.marjane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarjaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarjaneApplication.class, args);
+	}
+
+}
